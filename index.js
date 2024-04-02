8bfdc917f32a6410462b32b149ef9a12130abdee
Faiz Ali33 <faiz0651@gmail.com>
2024-04-03 00:16:35 +0500

    Add more features

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-console.log("\n\twellcome to codewithfaiz-CLI Number Guesting Game\n");
-const randomNumber = Math.floor(Math.random() * 6 + 1);
-const answer = await inquirer.prompt([
-    {
-        name: "userguestnumber",
-        type: "number",
-        message: "Enter Your Guess Number(Number limit from 1 to 6):",
-    },
-]);
-if (answer.userguestnumber === randomNumber) {
-    console.log("\nCongratulation ! You Guest A Correct Number>");
+let loop = true;
+while (loop) {
+    console.log("\n\twellcome to codewithfaiz-CLI Number Guesting Game\n");
+    const randomNumber = Math.floor(Math.random() * 6 + 1);
+    const answer = await inquirer.prompt([
+        {
+            name: "userguestnumber",
+            type: "number",
+            message: "Enter Your Guess Number(Number limit from 1 to 6):",
+        },
+    ]);
+    if (answer.userguestnumber === randomNumber) {
+        console.log("\nCongratulation ! You Guest A Correct Number>");
+    }
+    else {
+        console.log("\nSorry,You Guess A Wrong Number.\n");
+    }
+    const GuestingMore = await inquirer.prompt({
+        type: "confirm",
+        name: "more",
+        message: "Do You Want To More Guesting?",
+        default: false
+    });
+    if (!GuestingMore.more) {
+        loop = false;
+        console.log("\nThank You");
+    }
 }
-else {
-    console.log("\nSorry,You Guess A Wrong Number.");
-}
-console.log("\nThank You");
--- index.ts
+++ index.ts
@@ -2,21 +2,36 @@
 
 import inquirer from "inquirer";
 
-console.log("\n\twellcome to codewithfaiz-CLI Number Guesting Game\n");
+let loop = true;
 
-const randomNumber = Math.floor(Math.random() * 6 + 1);
+while (loop) {
+  console.log("\n\twellcome to codewithfaiz-CLI Number Guesting Game\n");
 
-const answer = await inquirer.prompt([
-  {
-    name: "userguestnumber",
-    type: "number",
-    message: "Enter Your Guess Number(Number limit from 1 to 6):",
-  },
-]);
+  const randomNumber = Math.floor(Math.random() * 6 + 1);
 
-if (answer.userguestnumber === randomNumber) {
-  console.log("\nCongratulation ! You Guest A Correct Number>");
-} else {
-  console.log("\nSorry,You Guess A Wrong Number.");
+  const answer = await inquirer.prompt([
+    {
+      name: "userguestnumber",
+      type: "number",
+      message: "Enter Your Guess Number(Number limit from 1 to 6):",
+    },
+  ]);
+
+  if (answer.userguestnumber === randomNumber) {
+    console.log("\nCongratulation ! You Guest A Correct Number.\n");
+  } else {
+    console.log("\nSorry,You Guess A Wrong Number.\n");
+  }
+
+  const GuestingMore = await inquirer.prompt({
+    type: "confirm",
+    name: "more",
+    message: "Do You Want To More Guesting?",
+    default: false,
+  });
+
+  if (!GuestingMore.more) {
+    loop = false;
+    console.log("\nThank You");
+  }
 }
-console.log("\nThank You")
